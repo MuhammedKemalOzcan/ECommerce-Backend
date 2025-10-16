31ee3c2ebf0ebe3394a1224b23b7c9cd003d4f04
unknown <muhammedkemal.57@gmail.com>
2025-10-16 16:31:13 +0300

    client layouts and pages added

---
+++ Ecommerce-Client/src/App.tsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Headphones from "./pages/Headphones";
+import Speakers from "./pages/Speakers";
+import Earphones from "./pages/Earphones";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/headphones", element: <Headphones /> },
+      { path: "/speakers", element: <Speakers /> },
+      { path: "/earphones", element: <Earphones /> },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
